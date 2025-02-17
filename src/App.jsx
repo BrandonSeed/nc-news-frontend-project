9c4ed273bebd17bc492290d8492044a8cfa0e077
BrandonSeed <brandon@seedshouse.co.uk>
2025-02-17 15:51:32 +0000

    add articles display

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
+import ArticlesDisplay from './components/ArticlesDisplay'
 
 function App() {
 
   return (
     <>
       <NavBar></NavBar>
+      <ArticlesDisplay></ArticlesDisplay>
     </>
   )
 }
