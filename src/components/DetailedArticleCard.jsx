bcb807ec898008c444de8a02ba983e06cf7291f1
BrandonSeed <brandon@seedshouse.co.uk>
2025-02-18 14:09:31 +0000

    refactor to redning loading message when waiting for article

--- src/components/DetailedArticleCard.jsx
+++ src/components/DetailedArticleCard.jsx
@@ -3,20 +3,26 @@ import { useState, useEffect } from "react"
 import getArticle from "../../utils/getArticle"
 
 function DetailedArticleCard() {
+    const [isLoading, setIsLoading] = useState(true)
     const [article, setArticle] = useState({})
-    const [postedAt, setPostedAt] = useState("")
     const { article_id } = useParams()
     useEffect(() => {
         getArticle(article_id).then((articleData) => {
             setArticle(articleData)
-            setPostedAt(`${articleData.created_at.slice(0,10)} at ${articleData.created_at.slice(11,16)}`)
+            setIsLoading(false)
         })
     }, [])
+
+    if (isLoading) {
+        return (
+            <p>Loading Article....</p>
+        )
+    }
 
     return (
         <>
             <h2>{article.title}</h2>
-            <p>Posted: {postedAt}</p>
+            <p>Posted: {article.created_at.slice(0,10)} at {article.created_at.slice(11,16)}</p>
             <p>Authored by: {article.author}</p>
             <img src={article.article_img_url} alt="realted article image" className="detailedArticleCardImg"/>
             <p>Votes: {article.votes}</p>
