4741574b7c46ef047a00c7d0ada6637b653f503b
BrandonSeed <brandon@seedshouse.co.uk>
2025-02-18 11:37:31 +0000

    adds path for single article

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import './App.css'
 import NavBar from './components/NavBar'
 import ArticlesDisplay from './components/ArticlesDisplay'
 import { Route, Routes } from 'react-router-dom'
+import Article from './components/Article'
 
 function App() {
 
   return (
     <>
-      <NavBar></NavBar>
+      <NavBar />
       <Routes>
-        <Route path="/" element={ <ArticlesDisplay /> }></Route>
-        
+        <Route path="/" element={ <ArticlesDisplay /> } />
+        <Route path="/articles/:article_id" element={ <Article />} />
 
       </Routes>
     </>
