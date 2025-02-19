c884f3cf27fba1bbd45d174a5533b919e404eedd
BrandonSeed <brandon@seedshouse.co.uk>
2025-02-19 16:49:40 +0000

    add state to update comments after posting

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
 import CommentsList from "./CommentsList"
 import DetailedArticleCard from "./DetailedArticleCard"
+import Addcomment from "./AddComment"
 
 function Article() {
+    const [updateComments, setUpdateComments] = useState(false)
     return (
         <>
             <DetailedArticleCard />
-            <CommentsList />
+            <Addcomment setUpdateComments={setUpdateComments}/>
+            <CommentsList updateComments={updateComments} setUpdateComments={setUpdateComments}/>
         </>
     )
 }
