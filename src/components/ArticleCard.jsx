889e18c6caa2ff75c3dc322dc8b3d3274523bd07
BrandonSeed <brandon@seedshouse.co.uk>
2025-02-19 11:00:55 +0000

    make minor change to titles of articles

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function ArticleCard({article}) {
     return (
         <>
-            <p>{article.title}</p>
+            <h2>{article.title}</h2>
             <img src={article.article_img_url} alt="varying images to draw attention to article" className="articleCardImg"/>
             <p>{article.topic}</p>
             <p>Posted: {article.created_at.slice(0,10)} at {article.created_at.slice(11,16)}</p>
